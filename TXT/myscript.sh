9a7c7cc1a2f2ac76abb75dac095ce615ab97f906
Pyqe Ponytails <67040405+Pyqe@users.noreply.github.com>
2022-09-20 11:52:15 +0700

    Perbaiki hostname, username, dan group, generate key ulang

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="458C879CE24B6F8E"
+REC2="33FA10DA4CBD4D67"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
